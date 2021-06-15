410973ffc9174036c66aa18322fbdd88bb6cb342
nils-holmberg <nils.holmberg@outlook.com>
2021-06-15 17:47:41 +0200

    computational content analyses, cognitive communication effects

---
+++ web/210928/visual.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import csv
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+import nltk
+from datetime import datetime
+import string
+import sys
+
+
+# In[2]:
+
+
+stop = nltk.corpus.stopwords.words("en") + list(string.punctuation)
+
+
+# In[ ]:
+
+
+
+
--- web/210930/readme.md
+++ web/210930/readme.md
@@ -1,10 +1,7 @@
 [home](https://nils-holmberg.github.io/sfac-py/)
 
-# natural language processing with spacy (210930)
+# some headline (2109)
 
-- install spacy 3.0 [explosion](https://github.com/explosion)
-- first nlp exercise [here](nlp.html)
-
-
+- some bullet point
 
 
