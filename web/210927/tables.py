d4288571e837a2d31e940f8d9c9516eb5352e1de
nils-holmberg <nils.holmberg@outlook.com>
2021-08-31 12:34:52 +0200

    computational content analyses, cognitive communication effects

--- web/210927/tables.py
+++ web/210927/tables.py
@@ -6,9 +6,31 @@
 # # python for research using the scipy stack (210927)
 # some line
 # 
+# ## overview day 1
+# 
+# - some intro
+# - easy start in google colab
+# - variables, flow control, con
+# - from ca 11-12 am, install anaconda3
+# 
+# ## some intro
+# 
+# - spyder ide, jupyter notebooks
+# - data structures, functions, pkgs
+# - navigate file system, working dir 
+# - **mindset: excel on steroids**
+# - file system, folders, csv files
+# - checking, cleaning, summarizing 
+# - sql-like operations on datasets
+# - combination of numeric and text 
+# - intro to day 2, nlp and spacy
+# 
+# <img src="../../fig/Microsoft_Office_Excel_2019_present.svg.png" width="500"/>
+
 # ## exercise 1
 # some line
 # 
+# ![caption](../../fig/612497595dc00-2-612344de79979__700.jpg)
 
 # In[1]:
 
