05457c6758b5a16f2c84eb6faffcf9ada0a16af9
nils-holmberg <nils.holmberg@outlook.com>
2021-09-14 09:21:21 +0200

    computational content analyses, cognitive communication effects

--- web/210927-sci/01-intro.py
+++ web/210927-sci/01-intro.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# [home](https://nils-holmberg.github.io/sfac-py/web/210927/)
+# [home](https://nils-holmberg.github.io/sfac-py/web/210927-sci/)
 # 
 # # day 1: getting started with python
 # - overview of day 1
 # 
-# |time  |section |concepts                   |outcomes                                                            |
-# |:-----|:-------|:--------------------------|:-------------------------------------------------------------------|
-# |09-10 |1.1.1   |[anaconda3](01-intro.html) |understand install location, create and update virtual environments |
-# |      |1.1.2   |jupyter                    |download course material, open notebooks, edit in web browser       |
-# |      |1.1.3   |testing                    |execute the universal hello world example in anaconda python env    |
-# |      |break   |                           |                                                                    |
-# |10-11 |1.2.1   |syntax                     |set variables, flow control                                         |
-# |      |1.2.2   |functions                  |understand modules                                                  |
-# |      |1.2.3   |files                      |read, write tabular data                                            |
-# |      |break   |                           |                                                                    |
-# |11-12 |1.3.1   |tables                     |                                                                    |
-# |      |1.3.2   |subset, substitute         |                                                                    |
-# |      |1.3.3   |summarize                  |                                                                    |
-# |12-13 |lunch   |anaconda, continued        |resolve any installation problems                                   |
+# |time  |section |concepts                        |outcomes                                                            |
+# |:-----|:-------|:-------------------------------|:-------------------------------------------------------------------|
+# |09-10 |1.1.1   |[anaconda3](01-intro.html)      |understand install location, create and update virtual environments |
+# |      |1.1.2   |jupyter                         |download course material, open notebooks, edit in web browser       |
+# |      |1.1.3   |testing                         |execute the hello world example in jupyter python env               |
+# |      |break   |                                |                                                                    |
+# |10-11 |1.2.1   |[python syntax](02-syntax.html) |set variables, flow control                                         |
+# |      |1.2.2   |functions                       |understand modules                                                  |
+# |      |1.2.3   |files, directories              |navigate the file system, read, write data files                    |
+# |      |break   |                                |                                                                    |
+# |11-12 |1.3.1   |[tabular data](03-tables.html)  |                                                                    |
+# |      |1.3.2   |subset, substitute              |                                                                    |
+# |      |1.3.3   |summarize                       |                                                                    |
+# |12-13 |lunch   |anaconda, continued             |try to resolve anaconda, psychopy installation problems             |
 # 
 # - **mindset: excel on steroids**
 # 
 # <img src="../../fig/Microsoft_Office_Excel_2019_present.svg.png" width="100"/>
 # 
-# ## section 1.1: anaconda
+# ## section 1.1.1: anaconda
 # - check anaconda3 installation (backup with google colab)
 # - anaconda console, conda update, create venv, install scipy
 # - python interpreter prompt, spyder ide, script editor
 # 
-# ## section 1.2: jupyter
+# ## section 1.1.2: jupyter
 # - download course [github repository](https://github.com/nils-holmberg/sfac-py.git)
 # - launch jupyter notebook, connect anaconda python kernel
 # 
-# ## section 1.3: testing
+# ## section 1.1.3: testing
 # - test interactive python commands in jupyter notebooks
 # - navigate file system, working dir 
 
--- web/210927-sci/02-syntax.py
+++ web/210927-sci/02-syntax.py
@@ -6,13 +6,13 @@
 # # day 1: getting started with python
 # - overview of day 1
 # 
-# ## section: 2.1: python
+# ## section: 1.2.1: python
 # - variables, flow control, con
 # 
-# ## section: 2.2: functions
+# ## section: 1.2.2: functions
 # - data structures, functions, pkgs
 # 
-# ## section: 2.3: files
+# ## section: 1.2.3: files
 # - navigate file system, working dir 
 # 
 
